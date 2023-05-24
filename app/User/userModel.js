6e8a1f27d9cc2da6fb5bac901f09349ead675577
Aouis CHOUKRI <78351892+Aouis20@users.noreply.github.com>
2023-05-24 14:39:57 +0200

    update user

---
+++ app/User/userModel.js
@@ -0,0 +1,27 @@
+// Import de Mongoose
+const mongoose = require('mongoose');
+
+// Création du schéma pour la collection "users"
+const userSchema = new mongoose.Schema({
+    lastname: { type: String, required: true },
+    age: {type: Number, min: [18,"L'age minimum est 18 ans, reçu: {VALUE}"], max: [100,"L'age maximum est 100 ans, reçu: {VALUE}"] },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+            validator: function (value) {
+                const emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+                return emailRegex.test(email);
+            },
+            message: props => `${props.value} n'est pas une adresse e-mail valide.`
+        }
+    },
+    password: { type: String, required: true, minLength: 8 },
+    gender: { type: String, required: true, enum: { values: ["male", "female"], message: "{VALUE} n'est pas possible"}}
+});
+
+// Création du modèle pour la collection "utilisateurs"
+const User = mongoose.model(User, userSchema);
+module.exports = User
+
--- app/models/user.js
+++
@@ -1,2 +0,0 @@
-// Création du modèle pour la collection "utilisateurs"
-const User = mongoose.model(User, userSchema);
