a8f3abf16c891e327b5edd5c4b19a6bc1ac44d5b
Aouis CHOUKRI <78351892+Aouis20@users.noreply.github.com>
2023-05-26 10:23:10 +0200

    start adding account

---
+++ app/routes/account.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+const accountCtrl = require("../controllers/account.js")
+const auth = require("../middleware/auth.js")
+
+router.post("/", auth, accountCtrl.create)
+router.put("/:id", auth, accountCtrl.update)
+router.delete("/:id", auth, accountCtrl.delete)
+router.delete("/:id", auth, accountCtrl.get)
